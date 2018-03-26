8df8631ea8c8c6e58a552ac9e208fc30be59e65f
Danny Gershman <danny.gershman@gmail.com>
2018-03-26 15:11:55 -0700

    formats and format resolution

--- run.php
+++ run.php
@@ -42,7 +42,6 @@ if ($result->num_rows > 0) {
                       "'" . $r["last_access_datetime"] . "'," .
                       "'" . $r["lang_enum"] . "')";
         $insert_result = $target_conn->query($insert_sql);
-        error_log($insert_result);
     }
 } else {
     echo "0 results";
@@ -71,7 +70,42 @@ if ($result->num_rows > 0) {
                       $r["sb_owner_2"] . "," .
                       "'" . $r["sb_meeting_email"] . "')";
         $insert_result = $target_conn->query($insert_sql);
-        error_log($insert_result);
+    }
+} else {
+    echo "0 results";
+}
+
+###formats
+$table_suffix = 'formats';
+resetMergeTable($table_suffix);
+$formats_max_id = getTargetMaxId($table_suffix, 'shared_id_bigint');
+$source_formats = getAllSourceData($table_suffix);
+$target_formats = getAllTargetData($table_suffix);
+createMergeTable($table_suffix);
+
+if ($source_formats->num_rows > 0) {
+    // output data of each row
+    while ( $r = $source_formats->fetch_assoc() ) {
+        $formatId = formatExists($target_formats, $r);
+        $notes = "exact match";
+        if ($formatId == 0) {
+            $notes = "unique format";
+            $formatId = $r["shared_id_bigint"] + $formats_max_id;
+            $insert_sql = "INSERT INTO " . $target_table_prefix . "comdef_" . $table_suffix . " VALUES (" .
+                          ($r["shared_id_bigint"] + $formats_max_id) . "," .
+                          "'" . $r["key_string"] . "'," .
+                          "NULL," .
+                          "'" . $r["worldid_mixed"] . "'," .
+                          "'" . $r["lang_enum"] . "'," .
+                          "'" . $r["name_string"] . "'," .
+                          "'" . $r["description_string"] . "'," .
+                          "'" . $r["format_type_enum"] . "')";
+            $insert_result = $target_conn->query($insert_sql);
+        }
+
+        if ($r["lang_enum"] == "en") {
+            insertIntoMergeTable($table_suffix, $r['shared_id_bigint'], $formatId, $notes);
+        }
     }
 } else {
     echo "0 results";
@@ -80,11 +114,12 @@ if ($result->num_rows > 0) {
 ### meetings
 $table_suffix = 'meetings_main';
 $meetings_main_max_id = getTargetMaxId($table_suffix, 'id_bigint');
-$result = getAllSourceData($table_suffix);
+$results = getAllSourceData($table_suffix);
 
-if ($result->num_rows > 0) {
+if ($results->num_rows > 0) {
     // output data of each row
-    while($r = $result->fetch_assoc()) {
+    while($r = $results->fetch_assoc()) {
+        $new_formats = getNewFormats($r["formats"]);
         $insert_sql = "INSERT INTO " . $target_table_prefix . "comdef_" . $table_suffix . " VALUES (" .
                       ($r["id_bigint"] + $meetings_main_max_id) . "," .
                       "'" . $r["worldid_mixed"] . "'," .
@@ -93,14 +128,13 @@ if ($result->num_rows > 0) {
                       $r["weekday_tinyint"] . "," .
                       "'" . $r["start_time"] . "'," .
                       "'" . $r["duration_time"] . "'," .
-                      "'" . $r["formats"] . "'," .
+                      "'" . $new_formats . "'," .
                       "'" . $r["lang_enum"] . "'," .
                       $r["longitude"] . "," .
                       $r["latitude"] . "," .
                       $r["published"] . "," .
                       "'" . $r["email_contact"] . "')";
         $insert_result = $target_conn->query($insert_sql);
-        error_log($insert_result);
     }
 } else {
     echo "0 results";
@@ -122,7 +156,6 @@ if ($result->num_rows > 0) {
                       "'" . $r["data_string"] . "'," .
                       "NULL,NULL)";
         $insert_result = $target_conn->query($insert_sql);
-        error_log($insert_result);
     }
 } else {
     echo "0 results";
@@ -148,11 +181,61 @@ function getAllSourceData($table_suffix) {
     return $GLOBALS['source_conn']->query("SELECT * FROM " . $GLOBALS['source_table_prefix'] . "comdef_" . $table_suffix);
 }
 
-function createMergeTable($table_suffix) {
-    $GLOBALS['source_conn']->query("CREATE TABLE lettuce_merge_" . $table_suffix . " (`old_id bigint`, `new_id bigint`)");
+function getAllTargetData($table_suffix) {
+    return $GLOBALS['target_conn']->query("SELECT * FROM " . $GLOBALS['target_table_prefix'] . "comdef_" . $table_suffix);
 }
 
-function mergeTableInsert($table_suffix, $old_id, $new_id) {
-    $merge_insert = "INSERT INTO lettuce_merge_" . $table_suffix . "(" . $old_id . "," . $new_id . ")";
+function resetMergeTable($table_suffix) {
+    $delete_table_sql = "DROP TABLE lettuce_merge_" . $table_suffix;
+    $GLOBALS['source_conn']->query($delete_table_sql);
+}
+
+function createMergeTable($table_suffix) {
+    $create_table_sql = "CREATE TABLE lettuce_merge_" . $table_suffix . " (old_id bigint, new_id bigint, notes varchar(255))";
+    $GLOBALS['source_conn']->query($create_table_sql);
+}
+
+function insertIntoMergeTable($table_suffix, $old_id, $new_id, $notes) {
+    $merge_insert = "INSERT INTO lettuce_merge_" . $table_suffix . " VALUES (" . $old_id . "," . $new_id . ",'". $notes . "')";
     $GLOBALS['source_conn']->query($merge_insert);
 }
+
+function formatExists($target_formats, $format_data) {
+    $target_formats->data_seek(0);
+    if ($target_formats->num_rows > 0) {
+        // output data of each row
+        while ( $r = $target_formats->fetch_assoc() ) {
+            if ($format_data['key_string'] == $r['key_string'] &&
+                $format_data['worldid_mixed'] == $r['worldid_mixed'] &&
+                $format_data['lang_enum'] == $r['lang_enum'] &&
+                $format_data['name_string'] == $r['name_string'] &&
+                $format_data['description_string'] == $r['description_string'] &&
+                $format_data['format_type_enum'] == $r['format_type_enum']) {
+                return $r['shared_id_bigint'];
+            }
+        }
+
+        return 0;
+    }
+}
+
+function getNewFormats($formats) {
+    if (strlen($formats) == 0) {
+        return "";
+    }
+    $formats_array = explode(",", $formats);
+    $new_formats_array = array();
+    foreach ($formats_array as $format_item) {
+        $format_lookup_sql = "SELECT new_id FROM lettuce_merge_formats WHERE old_id = " . $format_item;
+        $response = $GLOBALS['source_conn']->query($format_lookup_sql);
+        if ($response->num_rows > 0) {
+            // output data of each row
+            while ( $r = $response->fetch_assoc() ) {
+                array_push($new_formats_array, $r["new_id"]);
+            }
+        }
+    }
+
+    return implode(",", $new_formats_array);
+}
+
